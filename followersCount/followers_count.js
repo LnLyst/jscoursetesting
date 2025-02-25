6ce9d5b09565038417af4053cb4a52adfbf5621d
Lana Lyst <ln.lyst@gmail.com>
2025-02-25 04:18:43 -0500

    3 commit

--- followersCount/followers_count.js
+++ followersCount/followers_count.js
@@ -3,18 +3,24 @@ let count = 0; // Initialize count to 0
 
 function increaseCount() {
   count++; // Increment the count by 1
-  displayCount(count); // Display the count
-  checkCountValue(count); // Check count value and display messages
+  displayCount(); // Display the count
+  checkCountValue(); // Check count value and display messages
 }
 
-function displayCount(count) {
+function displayCount() {
   document.getElementById('countDisplay').innerHTML=count; // Display the count in the HTML
 }
 
-function checkCountValue(count) {
+function checkCountValue() {
   if (count === 10) {
     alert("Your Instagram post gained 10 followers! Congratulations!");
   } else if (count === 20) {
     alert("Your Instagram post gained 20 followers! Keep it up!");
   }
+}
+
+function resetCount() {
+    count=0;
+    document.getElementById('countDisplay').innerHTML=count;
+    alert("Your Instagram post has 0 followers! It was reset!");
 }
